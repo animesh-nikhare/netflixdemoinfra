8693e545ad9439a7de03ae88cd9cb712165adae7
animesh-nikhare <animesh.nikhare97@gmail.com>
2024-10-24 13:57:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-region = "us-east-1"
+region = "ap-south-2"
 }
 
 resource "aws_instance" "one" {
 count = 4
-ami = "ami-0ddc798b3f1a5117e"
+ami = "ami-0a74efd95b7028f59"
 instance_type = "t2.medium"
-key_name = "rahull"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+key_name = "hyd-key"
+vpc_security_group_ids = ["sg-0162a36cb83a1372d"]
 tags = {
 Name = var.instance_names[count.index]
 }
